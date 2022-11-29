cd2482ce8271a8dec3eb4fb8ce29ee668f169f98
wanderson-rpf <wandersonrpf@gmail.com>
2022-11-29 18:43:15 -0300

    :rocket: [deploy] Requisito 03 Concluído

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      {/* <Route exact path="/meals" component={  } />
+      <Route exact path="/drinks" component={  } />
+      <Route exact path="/meals/:id-da-receita" component={  } />
+      <Route exact path="/drinks/:id-da-receita" component={  } />
+      <Route exact path="/meals/:id-da-receita/in-progress" component={  } />
+      <Route exact path="/drinks/:id-da-receita/in-progress" component={  } />
+      <Route exact path="/profile" component={  } />
+      <Route exact path="/done-recipes" component={  } />
+      <Route exact path="/favorite-recipes" component={  } /> */}
     </Switch>
   );
 }
