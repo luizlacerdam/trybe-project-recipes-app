7460cbce8067e4a91483b13f3eb9bb5e2e1c0a5d
wanderson-rpf <wandersonrpf@gmail.com>
2022-11-30 20:15:04 -0300

    :construction: [progress] Requisitos 12 - Criando reducer drinks.

---
+++ src/redux/reducers/drinks.js
@@ -0,0 +1,25 @@
+import { RESPONSE_DRINKS_ERROR, RESPONSE_DRINKS_SUCCESS } from '../actions';
+
+const INIT_STATE = {
+  drinks: [],
+  error: null,
+};
+
+const drinks = (state = INIT_STATE, action) => {
+  switch (action.type) {
+  case RESPONSE_DRINKS_SUCCESS:
+    return {
+      ...state,
+      drinks: [...action.drinks],
+    };
+  case RESPONSE_DRINKS_ERROR:
+    return {
+      ...state,
+      error: action.error,
+    };
+  default:
+    return state;
+  }
+};
+
+export default drinks;
