b39d640dbb85c9b3b61a6cc3af305a2d6d25c636
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-12-08 08:14:16 -0300

    :sparkles: [feat] botão de compartilhar agora copiar o link da página e exibe mensagem no HTML requisito 33 OBS: Nova biblioteca instalada clipboard-copy

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+
+const copy = require('clipboard-copy');
 
 function ShareButton() {
-  return (
-    <button
-      data-testid="share-btn"
-      type="button"
-    >
-      SHARE
+  const history = useHistory();
+  const [linkMsg, setLinkMsg] = useState('');
+  const { location: { pathname } } = history;
 
-    </button>
+  const handleButton = () => {
+    setLinkMsg('Link copied!');
+    copy(`http://localhost:3000${pathname}`);
+  };
+  return (
+    <div>
+      <button
+        data-testid="share-btn"
+        type="button"
+        onClick={ handleButton }
+      >
+        <img src={ shareIcon } alt="share-icon" />
+
+      </button>
+      <div>{linkMsg}</div>
+    </div>
   );
 }
 
