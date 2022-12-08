3833b79bf464839ccc7fbf1aa4bf46928fea7859
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-12-08 15:00:59 -0300

    iniciando

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function FavoriteButton({ recipeProp }) {
   const history = useHistory();
   const { location: { pathname } } = history;
+  const [favoritesMeals, setFavoriteMeals] = useState([]);
+  const [favoritesDrinks, setFavoriteDrinks] = useState([]);
 
-  const handleButton = () => {
+  useEffect(() => {
     if (localStorage.getItem('favoriteRecipes') === null) {
       localStorage.setItem('favoriteRecipes', '[]');
+    } else {
+      const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      console.log(favoriteRecipes);
     }
+  }, []);
+  const handleButton = () => {
     const arr = JSON.parse(localStorage.favoriteRecipes);
     let newObj;
     if (pathname.includes('meals')) {
@@ -36,14 +45,13 @@ function FavoriteButton({ recipeProp }) {
     arr.push(newObj);
     localStorage.setItem('favoriteRecipes', JSON.stringify(arr));
   };
-  console.log(recipeProp);
   return (
     <button
       type="button"
       data-testid="favorite-btn"
       onClick={ handleButton }
     >
-      FAVORITE
+      <img src={ whiteHeartIcon } alt="" />
     </button>
   );
 }
