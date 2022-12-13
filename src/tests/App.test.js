0b31e211ecdce2c296bab78b27abe79aedac882f
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-12-13 17:44:57 -0300

    :bug: [fix] mudar o nome do arquivo InProgress.jsx para RecipeInProgress.jsx

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,28 @@
 // import React from 'react';
-// import { render, screen } from '@testing-library/react';
 // import App from '../App';
+// import categoriesListDrinksApi from '../services/categoriesListDrinksApi';
+// import { renderWithRouterAndRedux } from './helpers/renderWith';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  // render(<App />);
-  // const linkElement = screen.getByText(/TRYBE/i);
-  // expect(linkElement).toBeInTheDocument();
+describe('Testes de exceções nas Api"s:', () => {
+  describe('1. Testa serviço categoriesListDrinksApi.', () => {
+    // beforeEach(() => {
+    //   jest.spyOn(global, 'fetch').mockImplementation('error');
+    // });da
+
+    // afterEach(() => {
+    //   global.fetch.mockClear();
+    // });
+    test('1.1. Testa se houver error na resposta da Api.', async () => {
+      // const { history } = renderWithRouterAndRedux(
+      //   <App />,
+      //   { initialEntries: ['/meals'] },
+      // );
+      // try {
+      //   await categoriesListDrinksApi();
+      // } catch (e) {
+      //   expect(e).toMatch('error');
+      // }
+      // expect(await categoriesListDrinksApi()).rejects.toMatch('error');
+    });
+  });
 });
